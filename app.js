e78d8286cc459c577ae0f09c8e76823e2e8f81a8
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-06-25 23:19:58 -0400

    Add event listener to button method 3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const btn = document.querySelector('#btn');
 btn.onclick = () => alert("Hello World");
 
-const btn1 = document.querySelector('#btn');
+const btn1 = document.querySelector('#btn1');
 btn1.addEventListener('click', () => {
     alert("Hello World OwO");
 });
