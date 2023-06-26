b052fa3dec7a2f85dcfc3b3592f2c6d072cdb7d8
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-06-25 23:41:44 -0400

    Add method 4

--- app.js
+++ app.js
@@ -6,3 +6,8 @@ const btn1 = document.querySelector('#btn1');
 btn1.addEventListener('click', () => {
     alert("Hello World OwO");
 });
+
+//method 4
+function alertFunction() {
+    alert("YAAAAAY You done it :D");
+}
