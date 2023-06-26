4bcd64d5fd913bd62c71c25a9787ec1d0e875215
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-06-25 23:19:09 -0400

    Try to add event listener for button method 3

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 
 const btn = document.querySelector('#btn');
 btn.onclick = () => alert("Hello World");
+
+const btn1 = document.querySelector('#btn');
+btn1.addEventListener('click', () => {
+    alert("Hello World OwO");
+});
